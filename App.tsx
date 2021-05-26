889cab2aa9e8e0e563aeb908904d5027e0e5baa0
Eric Puskas <eric.puskas@yahoo.com>
2021-05-26 07:53:40 +0300

    Add react native paper and basic searchbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -8,17 +8,35 @@ import {
   Platform,
   StatusBar,
 } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   const isAndroid = Platform.OS === "android";
   const statusHeight = StatusBar.currentHeight;
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query: string) => setSearchQuery(query);
+
   return (
     <Fragment>
       <SafeAreaView
         style={{ flex: 1, marginTop: isAndroid ? statusHeight : 0 }}
       >
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
+        <View
+          style={{
+            padding: 16,
+            borderBottomWidth: 2,
+            borderTopWidth: 2,
+            borderBottomColor: "#f8f8f8",
+            borderTopColor: "#f8f8f8",
+          }}
+        >
+          <Searchbar
+            placeholder="Search"
+            value={searchQuery}
+            onChangeText={onChangeSearch}
+          />
         </View>
         <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
           <Text> List </Text>
